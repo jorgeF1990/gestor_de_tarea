72927dee03196bbc59b38be20100c9d3da10d9b0
Jorge Fernandez <142360088+jorgeF1990@users.noreply.github.com>
2026-06-22 15:10:37 -0300

    Fix: Agregar /debug-routes a api/index.js

--- api/index.js
+++ api/index.js
@@ -1,2 +1,21 @@
+console.log('=== API INDEX ===');
+console.log('Cargando backend/app.js...');
+
 const app = require('../backend/app.js');
+
+// Agregar ruta de debug
+app.get('/debug-routes', (req, res) => {
+  const routes = [];
+  app._router.stack.forEach(layer => {
+    if (layer.route) {
+      routes.push({
+        path: layer.route.path,
+        methods: Object.keys(layer.route.methods)
+      });
+    }
+  });
+  res.json({ routes });
+});
+
+console.log('App cargada correctamente');
 module.exports = app;
